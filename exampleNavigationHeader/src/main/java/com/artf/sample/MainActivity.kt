606c550c636d66561c918efb11a0a0dc77033cf6
ART_F <artur2113@o2.pl>
2019-07-06 02:14:44 +0200

    refactor samples

---
+++ exampleNavigationHeader/src/main/java/com/artf/sample/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.artf.sample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.artf.navigationheader.NavigationHeaderMotionLayout
+
+class MainActivity : AppCompatActivity() {
+
+    private val navigationHeader by lazy { findViewById<NavigationHeaderMotionLayout>(R.id.navigationHeader) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        window
+
+        val layoutInflater = LayoutInflater.from(this)
+        val headerList = mutableListOf<com.artf.navigationheader.HeaderView>()
+        val titleList = mutableListOf<com.artf.navigationheader.Header>().apply {
+            add(com.artf.navigationheader.Header("SERVICES", R.color.header1, R.color.statusBar1, R.color.content1))
+            add(com.artf.navigationheader.Header("AUTO", R.color.header2, R.color.statusBar2, R.color.content2))
+            add(com.artf.navigationheader.Header("JOB", R.color.header3, R.color.statusBar3, R.color.content3))
+            add(com.artf.navigationheader.Header("REALITY", R.color.header4, R.color.statusBar4, R.color.content4))
+        }
+
+        titleList.forEach {
+            val headerView = layoutInflater.inflate(R.layout.header, null)
+            headerView.findViewById<TextView>(R.id.title).text = it.title
+            headerView.background.clearColorFilter()
+            headerView.setBackgroundColor(ContextCompat.getColor(this, it.headerColor!!))
+
+            headerList.add(com.artf.navigationheader.HeaderView(headerView, it.statusBarColor, it.contentColor))
+        }
+
+        val contentList = mutableListOf<View>().apply {
+            add(findViewById(R.id.c1))
+            add(findViewById(R.id.c2))
+            add(findViewById(R.id.c3))
+            add(findViewById(R.id.c4))
+        }
+
+        navigationHeader.initNavigationHeader(this, headerList, contentList)
+    }
+}
