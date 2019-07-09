596f32fd29f9791f5b55df557405b5cd648a8808
ART_F <artur2113@o2.pl>
2019-07-09 06:05:13 +0200

    feat more variants

--- exampleNavigationHeader/src/main/java/com/artf/exampleNavigationHeader/MainActivity.kt
+++ exampleNavigationHeader/src/main/java/com/artf/exampleNavigationHeader/MainActivity.kt
@@ -23,12 +23,18 @@ class MainActivity : AppCompatActivity() {
         val title2 = resources.getString(R.string.header2)
         val title3 = resources.getString(R.string.header3)
         val title4 = resources.getString(R.string.header4)
+        val title5 = resources.getString(R.string.header5)
+        val title6 = resources.getString(R.string.header6)
+        val title7 = resources.getString(R.string.header7)
 
         val titleList = mutableListOf<Header>().apply {
             add(Header(title1, R.color.header1, R.color.statusBar1, R.color.content1))
             add(Header(title2, R.color.header2, R.color.statusBar2, R.color.content2))
             add(Header(title3, R.color.header3, R.color.statusBar3, R.color.content3))
             add(Header(title4, R.color.header4, R.color.statusBar4, R.color.content4))
+            add(Header(title5, R.color.header5, R.color.statusBar5, R.color.content5))
+            add(Header(title6, R.color.header6, R.color.statusBar6, R.color.content6))
+            add(Header(title7, R.color.header7, R.color.statusBar7, R.color.content7))
         }
 
         val layoutInflater = LayoutInflater.from(this)
